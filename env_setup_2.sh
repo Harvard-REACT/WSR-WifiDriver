f7b77c95e9348b6750fd634ef919a8d4a0cc89b5
up5 <njadhav@g.harvard.edu>
2022-08-02 12:32:58 -0400

    clean

--- env_setup_2.sh
+++ env_setup_2.sh
@@ -4,7 +4,8 @@ set -e
 cd ~
 cpuCores=`cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $NF}'`
 
-make -C WSR-Toolbox-linux-80211n-csitool-supplementary/netlink
+make clean -C ~/WSR-Toolbox-linux-80211n-csitool-supplementary/netlink
+make -C ~/WSR-Toolbox-linux-80211n-csitool-supplementary/netlink
 if [ ! -d "${HOME}/lorcon-old" ]
 then
 git clone https://github.com/dhalperi/lorcon-old.git
@@ -14,7 +15,8 @@ make -j $cpuCores
 sudo make install
 
 cd ~
-make -C WSR-Toolbox-linux-80211n-csitool-supplementary/injection
+make clean -C ~/WSR-Toolbox-linux-80211n-csitool-supplementary/injection
+make -C ~/WSR-Toolbox-linux-80211n-csitool-supplementary/injection
 
 
 echo "Setup completed."
